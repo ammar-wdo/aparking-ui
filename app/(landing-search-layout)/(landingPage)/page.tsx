7b27cfb272dd186b6c9b5a013eed7d76a92f3419
wdodigital 01 <wdodigital01@wdodigitals-Mac-mini.local>
2023-11-25 11:46:59 +0400

    to cancelation

--- app/(landing-search-layout)/(landingPage)/page.tsx
+++ app/(landing-search-layout)/(landingPage)/page.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link'
 import Banner from './(components)/banner'
 
 export default function Home() {
+
   return (
     <main className="">
       <Banner>
--- app/error.tsx
+++ app/error.tsx
@@ -1,5 +1,6 @@
 'use client' // Error components must be Client Components
  
+import { Button } from '@/components/ui/button'
 import Link from 'next/link'
 import { useEffect } from 'react'
  
@@ -16,19 +17,23 @@ export default function Error({
   }, [error])
  
   return (
-    <div>
-      <h2>Something went wrong!</h2>
+    <div className='h-screen flex items-center justify-center'>
+        <div className='space-y-4 text-center'>
+        <h2>Something went wrong!</h2>
+      <p className='py-4 text-rose-500'>{error.message}</p>
       <div className='flex items-center gap-4'>
-      <button
+      <Button
         onClick={
           // Attempt to recover by trying to re-render the segment
           () => reset()
         }
       >
         Try again
-      </button>
+      </Button>
       <Link href={'/'}>Back to main page</Link>
       </div>
+        </div>
+
     
     </div>
   )
