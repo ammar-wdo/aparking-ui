793675dedb33e865bc8c25b8b29da5998dda67a4
wdodigital 01 <wdodigital01@wdodigitals-Mac-mini.local>
2023-12-09 13:06:18 +0400

    UI

--- app/(landing-search-layout)/(airports)/[airportName]/[entityName]/[serviceName]/page.tsx
+++ app/(landing-search-layout)/(airports)/[airportName]/[entityName]/[serviceName]/page.tsx
@@ -1,30 +1,46 @@
-import Banner from '@/app/(landing-search-layout)/(landingPage)/(components)/banner'
-import { ALL_SERVICES } from '@/links'
-import { Service } from '@/schemas'
-import axios from 'axios'
-import { redirect } from 'next/navigation'
-import React from 'react'
+import Banner from "@/app/(landing-search-layout)/(landingPage)/(components)/banner";
+import { ALL_SERVICES } from "@/links";
+import { Service } from "@/schemas";
+import axios from "axios";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+import React from "react";
 
-type Props = {params:{serviceName:string}}
+type Props = { params: { serviceName: string } };
 
-const page =async ({params}: Props) => {
+const page = async ({ params }: Props) => {
+  const res = await axios(ALL_SERVICES + `/serviceInfo/${params.serviceName}`);
 
+  console.log(params.serviceName);
 
+  const service = res.data.service as Service & {
+    entity: { entityName: string; airport: { name: string } };
+  };
 
-    const res = await axios(ALL_SERVICES + `/serviceInfo/${params.serviceName}`)
-
-    console.log(params.serviceName)
-
-    const service = res.data.service as Service &{entity:{entityName:string,airport:{name:string}}}
-
-    if(!service) return redirect('/')
+  if (!service) return redirect("/");
   return (
     <div>
-          <Banner noForm>
-<h3 className='text-white text-3xl font-bold capitalize'>{service.name}</h3>
-        </Banner>
-    </div>
-  )
-}
+      <Banner noForm>
+        <h3 className="text-white text-3xl font-bold capitalize">
+          {service.name}
+        </h3>
+      </Banner>
 
-export default page
+      <div className="container mt-10">
+        <p className="text-neutral-500 flex items-center gap-4">
+          {" "}
+          <Link href={"/"}>Home</Link> &gt;{" "}
+          <Link href={`/${service?.entity?.airport.name}`}>
+            {service?.entity?.airport.name}
+          </Link>{" "}
+          &gt;{" "}
+          <Link href={`/${service?.entity?.airport.name}/${service?.entity.entityName}`}>{service?.entity.entityName}</Link>{" "}
+          &gt;{" "}
+          <span className="capitalize text-black">{service?.name}</span>{" "}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default page;
