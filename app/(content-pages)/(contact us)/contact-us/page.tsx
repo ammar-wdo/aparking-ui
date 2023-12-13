1b177c1d05abe3ae3127eb510f3a7c5d7adaf7dd
wdodigital 01 <wdodigital01@wdodigitals-Mac-mini.local>
2023-12-13 12:04:27 +0400

    UI

--- app/(content-pages)/(contact us)/contact-us/page.tsx
+++ app/(content-pages)/(contact us)/contact-us/page.tsx
@@ -5,7 +5,10 @@ import ContactForm from './(components)/contac-us-form'
 
 type Props = {}
 
-const page = (props: Props) => {
+const page =async (props: Props) => {
+
+
+
   return (
     <div className=''>
       <div className='container py-12 '>
