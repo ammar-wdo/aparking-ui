8914a1f5613a4f051f687db5f0f6218532fa3d9e
wdodigital 01 <wdodigital01@wdodigitals-Mac-mini.local>
2023-12-13 12:38:55 +0400

    UI

---
+++ app/(landing-search-layout)/(landingPage)/loading.tsx
@@ -0,0 +1,35 @@
+import { Skeleton } from '@/components/ui/skeleton'
+import { cn } from '@/lib/utils'
+import Image from 'next/image'
+import React from 'react'
+import AirportSkeleton from './(components)/airports-skeleton'
+
+type Props = {}
+
+const loading = (props: Props) => {
+  return (
+    <div className='min-h-[1200px] py-12'>
+        <div className={cn("md:min-h-[500px] min-h-[600px] items-center flex bg-[#003580] relative ")}>
+      <Image fill src='/Banner.jpg' alt="banner" className="object-cover opacity-10 object-center "/>
+      <div className='container h-full w-full gap-3  flex flex-col'>
+    <Skeleton className='p-4 w-[300px] my-3' />
+    <Skeleton className='p-12 w-full' />
+</div>
+
+    </div>
+    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-20 container'> 
+        <Skeleton className='h-[300px]' />
+        <Skeleton className='h-[300px]' />
+        <Skeleton className='h-[300px]' />
+
+        </div>
+
+        <AirportSkeleton />
+ 
+
+
+    </div>
+  )
+}
+
+export default loading
