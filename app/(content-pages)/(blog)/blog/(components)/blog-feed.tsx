91a1bd1d7f957ee5c497a9d19ca0000b27359a5d
wdodigital 01 <wdodigital01@wdodigitals-Mac-mini.local>
2023-12-15 11:22:43 +0400

    FAQ

--- app/(content-pages)/(blog)/blog/(components)/blog-feed.tsx
+++ app/(content-pages)/(blog)/blog/(components)/blog-feed.tsx
@@ -36,7 +36,7 @@ const BlogFeed = async ({ searchParams }: Props) => {
       )}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  xl:grid-cols-4 gap-8">
         {blogs.map((blog) => (
-          <BlogCard blog={blog} />
+          <BlogCard key={blog.id} blog={blog} />
        
         ))}
       </div>
