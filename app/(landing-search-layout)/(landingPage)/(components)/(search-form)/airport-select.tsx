042439188530da014ff279697eb3a040d070a744
wdodigital 01 <wdodigital01@wdodigitals-Mac-mini.local>
2023-12-16 09:20:54 +0400

    fix form

--- app/(landing-search-layout)/(landingPage)/(components)/(search-form)/airport-select.tsx
+++ app/(landing-search-layout)/(landingPage)/(components)/(search-form)/airport-select.tsx
@@ -7,8 +7,15 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
 
 import React, { Dispatch, SetStateAction } from "react";
+import { Button } from "@/components/ui/button";
+import { ChevronDown } from "lucide-react";
 
 type Props = {
   airports: { name: string; id: string }[];
@@ -27,37 +34,45 @@ const AirportSelect = ({
 }: Props) => {
 
   return (
-    <Select
-      onValueChange={setAirport}
-      defaultValue={airport}
+    <Popover
+      // onValueChange={setAirport}
+      // defaultValue={airport}
       onOpenChange={setOpen}
+      
       open={open}
     >
-      <SelectTrigger
+      <PopoverTrigger
+    
         className="w-full p-0 border-0 text-zinc-500
+        flex
+        items-center
       text-xs outline-none
        focus-visible:ring-0 
        focus-visible:ring-transparent 
        ring-0 pr-2 ring-transparent
         focus-within:ring-0 focus-within:ring-transparent
         font-semibold
+        h-full
+        rounded-md
          hover:bg-gray-100 transition foucs:ring-0 focus:ring-transparent 
          pl-4"
       >
-        <SelectValue className="" placeholder="Choose airport" />
-      </SelectTrigger>
-      <SelectContent>
+        <span  className="w-full  flex items-center justify-between">{airport && airports.find((airportE)=>airportE.id === airport)?.name || 'Choose an airport'} <ChevronDown className="w-4 h-4 text-neutral-400"/></span>
+        
+      </PopoverTrigger>
+      <PopoverContent className="flex flex-col ">
         {airports.map((airport) => (
-          <SelectItem
+          <Button
+          variant={'ghost'}
             key={airport.id}
-            value={airport.id}
-            className="cursor-pointer"
+            onClick={()=>setAirport(airport.id)}
+            className="cursor-pointer justify-start"
           >
             {airport.name}
-          </SelectItem>
+          </Button>
         ))}
-      </SelectContent>
-    </Select>
+      </PopoverContent>
+    </Popover>
   );
 };
 
--- app/(landing-search-layout)/(landingPage)/(components)/(search-form)/time-select.tsx
+++ app/(landing-search-layout)/(landingPage)/(components)/(search-form)/time-select.tsx
@@ -1,45 +1,58 @@
-'use client'
+"use client";
 
 
+import { ScrollArea } from "@/components/ui/scroll-area";
 import {
-    Select,
-    SelectContent,
-    SelectGroup,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-  } from "@/components/ui/select"
-  import { ScrollArea } from "@/components/ui/scroll-area"
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
 
-  
-  import React, { Dispatch, SetStateAction } from 'react'
-  
-  type Props = {
-    times:string[]
-    time:string,
-    setTime:Dispatch<SetStateAction<string>>
-    open:boolean,
-    setOpen:Dispatch<SetStateAction<boolean>>
-  }
-  
-  const TimeSelect = ({times,time,setTime,open,setOpen}: Props) => {
-    return (
-        <Select onValueChange={setTime} defaultValue={time} open={open} onOpenChange={setOpen}>
-      <SelectTrigger className="w-[180px] p-0 border-0 text-zinc-500 text-xs outline-none focus-visible:ring-0 focus-visible:ring-transparent ring-0 ring-transparent focus-within:ring-0 focus-within:ring-transparent hover:bg-gray-100 transition foucs:ring-0 focus:ring-transparent pl-4">
-        <SelectValue  placeholder="Select Time" />
-      </SelectTrigger>
-      
-      <SelectContent>
-        <ScrollArea className="h-[200px] w-fit rounded-md  p-4">
-       {times.map((time=>
-         <SelectItem key={time} className="cursor-pointer w-fit" value={time}>{time}</SelectItem>))}
+import React, { Dispatch, SetStateAction } from "react";
+import { ChevronDown } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
+type Props = {
+  times: string[];
+  time: string;
+  setTime: Dispatch<SetStateAction<string>>;
+  open: boolean;
+  setOpen: Dispatch<SetStateAction<boolean>>;
+};
+
+const TimeSelect = ({ times, time, setTime, open, setOpen }: Props) => {
+  return (
+    <Popover
+      // onValueChange={setTime}
+      // defaultValue={time}
+      open={open}
+      onOpenChange={setOpen}
+    >
+      <PopoverTrigger  className="w-[180px] p-0 border-0 h-full rounded-md px-1
+       text-zinc-500 text-xs outline-none focus-visible:ring-0 
+       focus-visible:ring-transparent ring-0 ring-transparent focus-within:ring-0
+        focus-within:ring-transparent hover:bg-gray-100 transition foucs:ring-0 
+        focus:ring-transparent pl-4">
+       <span className="w-full  flex items-center justify-between">{time || "Select time"} <ChevronDown className="w-4 h-4 text-neutral-400"/></span>
+      </PopoverTrigger>
+
+      <PopoverContent className="w-fit">
+        <ScrollArea className="h-[200px] w-fit rounded-md  p-4 flex flex-col">
+          {times.map((time) => (
+            <Button
+            variant={'ghost'}
+            key={time}
+            onClick={()=>setTime(time)}
+            className="cursor-pointer justify-start block"
+          >
           
-          </ScrollArea>
-         
-        
-      </SelectContent>
-    </Select>
-    )
-  }
-  
-  export default TimeSelect
+              {time}
+            </Button>
+          ))}
+        </ScrollArea>
+      </PopoverContent>
+    </Popover>
+  );
+};
+
+export default TimeSelect;
