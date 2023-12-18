b9417ebdd28f071e527965c4c919c6588a7ad99f
wdodigital 01 <wdodigital01@wdodigitals-Mac-mini.local>
2023-12-18 09:48:15 +0400

    airport scroll

--- app/(landing-search-layout)/(landingPage)/(components)/airports.tsx
+++ app/(landing-search-layout)/(landingPage)/(components)/airports.tsx
@@ -21,10 +21,10 @@ const Airports = async (props: Props) => {
       <h3 className="text-center text-4xl font-semibold text-[#003580] ">
         Airports
       </h3>
-      <div className="max-w-[1500px] mx-auto mt-6 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10 p-4">
+      <div className="max-w-[1500px] mx-auto mt-6  lg:grid-cols-4 md:grid-cols-3 hidden md:grid gap-10 p-4">
         {res.data.airports.map((airport: Airport) => (
           <Link key={airport.id} href={`/${airport.name}`}>
-          <div  className="rounded-md overflow-hidden shadow-sm hover:shadow-md transition">
+          <div  className="rounded-md overflow-hidden shadow-sm hover:shadow-md transition flex flex-col h-full">
             <div className="relative aspect-video  ">
               <Image
                 fill
@@ -33,7 +33,27 @@ const Airports = async (props: Props) => {
                 className="object-cover "
               />
             </div>
-            <div className="bg-white p-4">
+            <div className="bg-white p-4 flex-1">
+<h3 className="text-center text-[#003580] text-lg font-semibold">{airport.name}</h3>
+<p className="text-xs text-gray-400 text-center pt-3">vanaf € 32,00 per week</p>
+            </div>
+          </div>
+          </Link>
+        ))}
+      </div>
+      <div className="md:hidden flex items-stretch gap-4 overflow-x-auto w-full relative z-50 p-8">
+      {res.data.airports.map((airport: Airport) => (
+          <Link key={airport.id} href={`/${airport.name}`} className="flex-shrink-0 max-w-[270px] w-full  ">
+          <div  className="rounded-md overflow-hidden shadow-sm hover:shadow-md transition flex flex-col h-full">
+            <div className="relative aspect-video  ">
+              <Image
+                fill
+                src={airport.images[0]}
+                alt="airport-image"
+                className="object-cover "
+              />
+            </div>
+            <div className="bg-white p-4 flex-1">
 <h3 className="text-center text-[#003580] text-lg font-semibold">{airport.name}</h3>
 <p className="text-xs text-gray-400 text-center pt-3">vanaf € 32,00 per week</p>
             </div>
