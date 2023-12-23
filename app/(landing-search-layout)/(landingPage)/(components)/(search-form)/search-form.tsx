d45921cab0136117c445082a5cb7db86452cf65f
wdodigital 01 <wdodigital01@wdodigitals-Mac-mini.local>
2023-12-23 13:26:02 +0400

    UI

--- app/(landing-search-layout)/(landingPage)/(components)/(search-form)/search-form.tsx
+++ app/(landing-search-layout)/(landingPage)/(components)/(search-form)/search-form.tsx
@@ -11,25 +11,29 @@ import Filter from "@/components/sheets/filter";
 import { Button } from "@/components/ui/button";
 import { Loader } from "lucide-react";
 
-
 type Props = {
-  startDateProp?:Date,
-  endDateProp?:Date,
-  startTimeProp?:string,
-  endTimeProp?:string,
-  change?:boolean,
-  airports?:{id:string,name:string,slug:string}[]
-  airportProp?:string,
-  airportSlug?:string
-  serviceId?:string
-
-
+  startDateProp?: Date;
+  endDateProp?: Date;
+  startTimeProp?: string;
+  endTimeProp?: string;
+  change?: boolean;
+  airports?: { id: string; name: string; slug: string }[];
+  airportProp?: string;
+  airportSlug?: string;
+  serviceId?: string;
 };
 
-const SearchForm = ({startDateProp,endDateProp,startTimeProp,endTimeProp,change,airports,airportProp,airportSlug,serviceId}: Props) => {
- 
-
-
+const SearchForm = ({
+  startDateProp,
+  endDateProp,
+  startTimeProp,
+  endTimeProp,
+  change,
+  airports,
+  airportProp,
+  airportSlug,
+  serviceId,
+}: Props) => {
   const {
     airport,
     setAirport,
@@ -50,59 +54,110 @@ const SearchForm = ({startDateProp,endDateProp,startTimeProp,endTimeProp,change,
     setOpenStartTime,
     openEndTime,
     setOpenEndTime,
-    setStartTime,setEndTime,
+    setStartTime,
+    setEndTime,
     handleClick,
-    isLoading
-  } = useSearchForm({startDateProp,endDateProp,startTimeProp,endTimeProp,airportProp,change,airportSlug,serviceId});
-
-
+    isLoading,
+  } = useSearchForm({
+    startDateProp,
+    endDateProp,
+    startTimeProp,
+    endTimeProp,
+    airportProp,
+    change,
+    airportSlug,
+    serviceId,
+  });
 
   return (
     <div className="text-white ">
-      {<div className={cn("fixed inset-0 opacity-0 duration-300 bg-black/60 delay-100",(openStart||openEnd||openStartTime||openEndTime||openAirport) ? 'opacity-100 z-20':'-z-10')}></div>}
-   
+      {
+        <div
+          className={cn(
+            "fixed inset-0 opacity-0 duration-300 bg-black/60 delay-100",
+            openStart || openEnd || openStartTime || openEndTime || openAirport
+              ? "opacity-100 z-20"
+              : "-z-10"
+          )}
+        ></div>
+      }
 
       <section className="rounded-xl flex lg:flex-row flex-col overflow-hidden mt-10 gap-1 relative z-30">
-        <div className={cn("grid lg:grid-cols-3 flex-1 gap-1",(airportSlug || serviceId )&& 'lg:grid-cols-2')}>
-          {!airportSlug && !serviceId&& !!airports && !!airports.length&&<div className="p-2 bg-white flex flex-col gap-1 pb-1">
-          <h3 className="text-black font-semibold pl-2">Airport</h3>
-           <AirportSelect airport={airport!} setAirport={setAirport} airports={airports} open={openAirport} setOpen={setOpenAirport}/>
-          </div>}
+        <div
+          className={cn(
+            "grid lg:grid-cols-3 flex-1 gap-1",
+            (airportSlug || serviceId) && "lg:grid-cols-2"
+          )}
+        >
+          {!airportSlug && !serviceId && !!airports && !!airports.length && (
+            <div className="p-2 bg-white flex flex-col gap-1 pb-1">
+              <h3 className="text-black font-semibold pl-2">Airport</h3>
+              <AirportSelect
+                airport={airport!}
+                setAirport={setAirport}
+                airports={airports}
+                open={openAirport}
+                setOpen={setOpenAirport}
+              />
+            </div>
+          )}
           <div className="p-2 bg-white flex flex-col gap-1 pb-1">
             <h3 className="text-black font-semibold">From</h3>
             <div className="flex items-center gap-2">
               {" "}
               <DatePicker
-              
                 open={openStart}
                 setOpen={setOpenStart}
                 date={startDate}
-                
                 setDate={setStartDate}
               />
               <TimeSelect
-              open={openStartTime} setOpen={setOpenStartTime} time={startTime} setTime={setStartTime} times={timeArray} />
+              showAll
+                startDate={startDate}
+                endDate={endDate}
+                startTime={startTime}
+                endTime={endTime}
+                open={openStartTime}
+                setOpen={setOpenStartTime}
+                time={startTime}
+                setTime={setStartTime}
+                times={timeArray}
+              />
             </div>
           </div>
           <div className="p-2 bg-white flex flex-col gap-1 pb-1">
             <h3 className="text-black font-semibold">To</h3>
             <div className="flex items-center gap-2">
-            <DatePicker
-              open={openEnd}
-              setOpen={setOpenEnd}
-              fromDate={startDate}
-              date={endDate}
-              setDate={setEndDate}
-              
-            />
-            <TimeSelect open={openEndTime} setOpen={setOpenEndTime} time={endTime} setTime={setEndTime} times={timeArray} />
+              <DatePicker
+                open={openEnd}
+                setOpen={setOpenEnd}
+                fromDate={startDate}
+                date={endDate}
+                setDate={setEndDate}
+              />
+              <TimeSelect
+                open={openEndTime}
+                setOpen={setOpenEndTime}
+                startTime={startTime}
+                endTime={endTime}
+                time={endTime}
+                setTime={setEndTime}
+                times={timeArray}
+                startDate={startDate}
+                endDate={endDate}
+              />
             </div>
-           
           </div>
         </div>
-        <button disabled={isLoading} onClick={handleClick}  className="px-8 flex items-center justify-center disabled:opacity-50 disabled:cursor-default bg-[#FEBA02] hover:bg-[#FEBA02]/90 transition  text-[#003580] font-semibold  capitalize py-3   lg:py-0 rounded-xl lg:rounded-l-none rounded-tl-none rounded-tr-none lg:rounded-tr-xl">
-         {change ?  "Change":"Search"}
-         {isLoading && <Loader className="ml-3 h-3 w-3 text-[#003580] animate-spin" />}
+        <button
+          disabled={isLoading}
+          onClick={handleClick}
+          className="px-8 flex items-center justify-center disabled:opacity-50 disabled:cursor-default bg-[#FEBA02] hover:bg-[#FEBA02]/90 transition  text-[#003580] font-semibold  capitalize py-3   lg:py-0 rounded-xl lg:rounded-l-none rounded-tl-none rounded-tr-none lg:rounded-tr-xl"
+        >
+          {change ? "Change" : "Search"}
+          {isLoading && (
+            <Loader className="ml-3 h-3 w-3 text-[#003580] animate-spin" />
+          )}
         </button>
         {change && <Filter />}
       </section>
--- app/(landing-search-layout)/(landingPage)/(components)/(search-form)/time-select.tsx
+++ app/(landing-search-layout)/(landingPage)/(components)/(search-form)/time-select.tsx
@@ -1,16 +1,16 @@
 "use client";
 
-
 import { ScrollArea } from "@/components/ui/scroll-area";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
-} from "@/components/ui/popover"
+} from "@/components/ui/popover";
 
 import React, { Dispatch, SetStateAction } from "react";
 import { ChevronDown } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { getClientDates } from "@/app/(checkout)/checkout/[serviceId]/update/(helpers)/getClientDates";
 
 type Props = {
   times: string[];
@@ -18,9 +18,25 @@ type Props = {
   setTime: Dispatch<SetStateAction<string>>;
   open: boolean;
   setOpen: Dispatch<SetStateAction<boolean>>;
+  startDate: Date | undefined;
+  endDate: Date | undefined;
+showAll?:boolean
+  startTime: string;
+  endTime: string;
 };
 
-const TimeSelect = ({ times, time, setTime, open, setOpen }: Props) => {
+const TimeSelect = ({
+  times,
+  time,
+  setTime,
+  open,
+  setOpen,
+  startDate,
+  endDate,
+  startTime,
+  endTime,
+  showAll
+}: Props) => {
   return (
     <Popover
       // onValueChange={setTime}
@@ -28,27 +44,69 @@ const TimeSelect = ({ times, time, setTime, open, setOpen }: Props) => {
       open={open}
       onOpenChange={setOpen}
     >
-      <PopoverTrigger asChild  className="w-[180px] p-0 border-0 h-full rounded-md px-1
+      <PopoverTrigger
+        asChild
+        className="w-[180px] p-0 border-0 h-full rounded-md px-1
        text-gray-600 text-xs outline-none focus-visible:ring-0 
        focus-visible:ring-transparent ring-0 ring-transparent focus-within:ring-0
         focus-within:ring-transparent hover:bg-slate-100 transition foucs:ring-0 
-        focus:ring-transparent pl-4">
-       <Button variant={'ghost'} className="w-full  flex items-center justify-between">{time || "Select time"} <ChevronDown className="w-4 h-4 text-gray-600"/></Button>
+        focus:ring-transparent pl-4"
+      >
+        <Button
+          variant={"ghost"}
+          className="w-full  flex items-center justify-between"
+        >
+          {time || "Select time"}{" "}
+          <ChevronDown className="w-4 h-4 text-gray-600" />
+        </Button>
       </PopoverTrigger>
 
       <PopoverContent className="w-fit">
         <ScrollArea className="h-[200px] w-fit rounded-md  p-4 flex flex-col">
-          {times.map((time) => (
-            <Button
-            variant={'ghost'}
-            key={time}
-            onClick={()=>{setTime(time);setOpen(false)}}
-            className="cursor-pointer justify-start block"
-          >
-          
+          {times.map((time) => {
+
+            if(endDate && startDate && !showAll){
+              const {clientArrivalDate,clientDepartureDate} = getClientDates(
+                startDate?.toString() || new Date().toString(),
+                endDate?.toString() || new Date().toString(),
+                startTime,
+                endTime
+              );
+  const [hours,minutes] = time.split(":")
+              endDate?.setHours(+hours)
+              endDate?.setMinutes(+minutes)
+  
+              if (clientArrivalDate.getTime()  >= endDate?.getTime() ) {
+                return null
+              }
+
+              return   <Button
+              variant={"ghost"}
+              key={time}
+              onClick={() => {
+                setTime(time);
+                setOpen(false);
+              }}
+              className="cursor-pointer justify-start block"
+            >
               {time}
             </Button>
-          ))}
+            }
+        
+              return (
+                <Button
+                  variant={"ghost"}
+                  key={time}
+                  onClick={() => {
+                    setTime(time);
+                    setOpen(false);
+                  }}
+                  className="cursor-pointer justify-start block"
+                >
+                  {time}
+                </Button>
+              );
+          })}
         </ScrollArea>
       </PopoverContent>
     </Popover>
