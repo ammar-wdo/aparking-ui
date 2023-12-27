f7c4e28b69389ade49f266720833f4ff2c8232d4
wdodigital 01 <wdodigital01@wdodigitals-Mac-mini.local>
2023-12-27 13:57:09 +0400

    time zone

--- schemas.ts
+++ schemas.ts
@@ -13,7 +13,7 @@ export const bookingSchema = z
 
     firstName: z.string().min(1,{message:'First name is required'}),
     lastName: z.string().min(1,{message:'Last name is required'}),
-    email: z.string().email({message:"E-mail is required"}),
+    email: z.string().email({message:"E-mail is required"}).min(1,{message:"E-mail is required"}),
     carColor: z.string().min(1,{message:'Car color is required'}),
     carLicense: z.string().min(1,{message:'Car license is required'}),
     carModel: z.string().min(1,{message:'Car model is required'}),
