df806d7b9e3872cb8bd14a28ad791d8d335d3e0a
wdodigital 01 <wdodigital01@wdodigitals-Mac-mini.local>
2024-01-05 12:59:28 +0400

    total reviews

--- app/(landing-search-layout)/(airports)/[airportName]/[entityName]/(components)/services-feed.tsx
+++ app/(landing-search-layout)/(airports)/[airportName]/[entityName]/(components)/services-feed.tsx
@@ -18,7 +18,7 @@ const ServciesFeed = async({entityId}: Props) => {
     <div className='p-3 bg-gray-100 py-10'>
       {!services.length && <p className='py-6 text-3xl font-bold text-neutral-400 text-center'>No parking services</p>}
       <div className='container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
-{services.map((service :Service &{parkingDays?:number,totalPrice?:number,entity:{entityName:string,slug:string,airport:{name:string,slug:string}}})=><ListCard key={service.id} show service={service} />)}
+{services.map((service :Service &{parkingDays?:number,totalPrice?:number,entity:{entityName:string,slug:string,airport:{name:string,slug:string}},totalReviews:number})=><ListCard key={service.id} show service={service} />)}
       </div>
     </div>
   )
