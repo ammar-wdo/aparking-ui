cabc8683cb9dc1f1f3d1d26b07f1e3280c3a884b
wdodigital 01 <wdodigital01@wdodigitals-Mac-mini.local>
2024-01-09 10:55:56 +0400

    static

---
+++ app/(landing-search-layout)/(landingPage)/test.tsx
@@ -0,0 +1,36 @@
+// import { Skeleton } from '@/components/ui/skeleton'
+// import { cn } from '@/lib/utils'
+// import Image from 'next/image'
+// import React from 'react'
+// import AirportSkeleton from './(components)/airports-skeleton'
+
+// type Props = {}
+
+// const loading = (props: Props) => {
+//   return (
+//     <div className='min-h-[1200px] pb-12'>
+//         <div className={cn("md:min-h-[500px] min-h-[600px] items-center flex bg-[#003580] relative ")}>
+//       <Image fill src='/Banner.jpg' alt="banner" className="object-cover opacity-10 object-center "/>
+//       <div className='container h-full w-full gap-3  flex flex-col'>
+//     <Skeleton className='p-4 w-[300px] my-3' />
+//     <Skeleton className='p-12 w-full' />
+// </div>
+
+//     </div>
+//     <h3 className='text-center text-4xl font-semibold text-[#003580] mt-12'>How does it work?</h3>
+//     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-6 container'> 
+//         <Skeleton className='h-[300px]' />
+//         <Skeleton className='h-[300px]' />
+//         <Skeleton className='h-[300px]' />
+
+//         </div>
+
+//         <AirportSkeleton />
+ 
+
+
+//     </div>
+//   )
+// }
+
+// export default loading
