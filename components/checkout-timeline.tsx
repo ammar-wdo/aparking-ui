56aa46f1a9e157d9f3aa345bfcca31460d12cfd8
wdodigital 01 <wdodigital01@wdodigitals-Mac-mini.local>
2024-01-11 11:08:46 +0400

    checkout

--- components/checkout-timeline.tsx
+++ components/checkout-timeline.tsx
@@ -30,8 +30,8 @@ const CheckoutTimeline = ({valet}: Props) => {
     <div className='container px-[16px]'>
         <div className=' bg-white p-6'>
             <h3 className='text-2xl font-bold border-b pb-6'>What happens next?</h3>
-            <div className='flex lg:items-center justify-between gap-2 mt-12 flex-col lg:flex-row relative'>
-                <div className='hidden lg:block z-10 absolute h-4 w-full border-t border-dashed  border-site left-0 origin-center  '></div>
+            <div className='flex  justify-between gap-2 mt-12 flex-col lg:flex-row relative'>
+                <div className='hidden lg:block z-10 absolute h-4 w-full border-t border-dashed top-14  border-site left-0 origin-center  '></div>
                 {elements.map(el=><div key={el.title} className='flex lg:flex-col items-center text-center gap-6 relative z-20 bg-white p-2 lg:py-8 rounded-2xl lg:border'>
                     <span className='flex items-center justify-center bg-site rounded-xl w-12 h-12 text-white shrink-0'>{el.icon}</span>
                     <p className='text-sm lg:max-w-[200px]'>{el.title}</p>
