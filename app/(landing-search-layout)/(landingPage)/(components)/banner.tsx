a491106e1f76f724473b8cc2fb910cc83d624492
wdodigital 01 <wdodigital01@wdodigitals-Mac-mini.local>
2024-01-12 14:35:58 +0400

    refine

--- app/(landing-search-layout)/(landingPage)/(components)/banner.tsx
+++ app/(landing-search-layout)/(landingPage)/(components)/banner.tsx
@@ -5,31 +5,68 @@ import { cn } from "@/lib/utils";
 import axios from "axios";
 import { GET_AIRPORTS } from "@/links";
 import Image from "next/image";
+import { Check } from "lucide-react";
 
-type Props = {children?:React.ReactNode
-noForm?:boolean,
-airportSlug?:string,
-airportName?:string
+type Props = {
+  children?: React.ReactNode;
+  noForm?: boolean;
+  airportSlug?: string;
+  airportName?: string;
 };
 
-const Banner =async ({children,noForm,airportSlug,airportName}: Props) => {
-  const res = await axios.get(GET_AIRPORTS)
- 
+const Banner = async ({
+  children,
+  noForm,
+  airportSlug,
+  airportName,
+}: Props) => {
+  const res = await axios.get(GET_AIRPORTS);
+  const highlights = [
+    "Gratis annuleren/wijzigen",
+    "Parkeerplaatsen geïnspecteerd",
+    "Geen reserveringskosten",
+    "Altijd laagste prijs",
+  ];
 
   return (
-    <div className={cn("md:min-h-[500px] min-h-[400px] py-12 items-center flex bg-[#003580] relative ",noForm && 'min-h-[500px] ')}>
-      <Image fill src='/Banner.jpg' alt="banner" className="object-cover opacity-10 object-center "/>
-      <div className={"container "} >
-      
-        <div className={cn(" text-white relative ",noForm && "xl:px-0")}>
-          {airportName && <p className="text-4xl py-3 font-semibold capitalize">{airportName}</p>}
-        <div>
-          {children}
+    <div
+      className={cn(
+        "md:min-h-[500px] min-h-[400px] py-12 items-center flex bg-[#003580] relative ",
+        noForm && "min-h-[500px] "
+      )}
+    >
+      <Image
+        fill
+        src="/Banner.jpg"
+        alt="banner"
+        className="object-cover opacity-10 object-center "
+      />
+      <div className={"container "}>
+        <div className={cn(" text-white relative ", noForm && "xl:px-0")}>
+          {airportName && (
+            <p className="text-4xl py-3 font-semibold capitalize">
+              {airportName}
+            </p>
+          )}
+          <div>{children}</div>
+
+          {!noForm && (
+            <SearchForm
+              airports={res.data.airports}
+              airportSlug={airportSlug}
+            />
+          )}
+          {!noForm && (
+            <div className="mt-8 flex items-center gap-6 flex-wrap">
+              {highlights.map((el) => (
+                <span className="flex items-center gap-1" key={el}>
+                  <Check className="w-4 h-4 text-white" />
+                  {el}
+                </span>
+              ))}
+            </div>
+          )}
         </div>
-      
-       { !noForm && <SearchForm airports={res.data.airports} airportSlug={airportSlug} />}
-        </div>
-      
       </div>
     </div>
   );
