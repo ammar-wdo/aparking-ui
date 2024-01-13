a11a666ba713b54cd253846597f5595e348134a1
wdodigital 01 <wdodigital01@wdodigitals-Mac-mini.local>
2024-01-13 13:09:17 +0400

    refine

--- app/(landing-search-layout)/(landingPage)/(components)/airports-swiper.tsx
+++ app/(landing-search-layout)/(landingPage)/(components)/airports-swiper.tsx
@@ -50,6 +50,28 @@ const AirportsSwiper = ({ airports }: Props) => {
       spaceBetween={20}
       slidesPerView={4}
       navigation
+      breakpoints={{
+        // when window width is >= 320px
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 20
+        },
+        // when window width is >= 640px
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20
+        },
+        // when window width is >= 768px
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 20
+        },
+        // when window width is >= 1024px
+        1024: {
+          slidesPerView: 4,
+          spaceBetween: 20
+        }
+      }}
     >
       {airports.map((airport) => (
         <SwiperSlide key={airport.id} className="w-[300px]">
