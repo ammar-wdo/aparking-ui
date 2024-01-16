c09e6fbaec6023bf6ace745addafeaf1e72b0121
wdodigital 01 <wdodigital01@wdodigitals-Mac-mini.local>
2024-01-16 11:55:17 +0400

    refine

--- app/(landing-search-layout)/(landingPage)/(components)/explain.tsx
+++ app/(landing-search-layout)/(landingPage)/(components)/explain.tsx
@@ -1,4 +1,5 @@
 import { Car, MousePointerClick, SearchCheck } from "lucide-react";
+import Image from "next/image";
 import React from "react";
 
 type Props = {};
@@ -9,25 +10,25 @@ const Explain = (props: Props) => {
       title: "Reserveer Online",
       description:
         "Maak eenvoudig een reservering en ontvang duidelijke instructies per e-mail.",
-      icon: <SearchCheck className="text-white w-7 h-7" />,
+      icon: '/icon1.png',
     },
     {
       title: "Rijd Naar het Vliegveld",
       description:
         "Volg onze aangegeven route en specifieke aanwijzingen, uniek per parkeeraanbieder.",
-      icon: <MousePointerClick className="text-white w-7 h-7" />,
+        icon: '/icon2.png',
     },
     {
       title: "Auto-overdracht",
       description:
         "Check in bij de parkeeraanbieder, neem de gratis shuttlebus naar het vliegveld, of kies voor valet parkeren bij de vertrekhal.",
-      icon: <Car className="text-white w-7 h-7" />,
+        icon: '/icon3.png',
     },
     {
       title: "Welkom Terug",
       description:
         "Bij terugkeer, neem de shuttlebus terug naar het parkeerterrein of wacht bij de aankomsthal als je valet parkeert. Je auto staat klaar om je direct naar huis te brengen.",
-      icon: <Car className="text-white w-7 h-7" />,
+        icon: '/icon4.png',
     },
   ];
 
@@ -36,13 +37,13 @@ const Explain = (props: Props) => {
       <h3 className="text-center text-2xl sm:text-4xl font-semibold text-[#003580] ">
         Parkeer Slim in 4 Stappen met Aparking
       </h3>
-      <div className="mt-8 flex gap-4  justify-between overflow-x-auto p-2 noScroll xl:grid xl:grid-cols-4">
+      <div className="mt-8 flex gap-4  justify-between overflow-x-auto p-2 noScroll xl:grid xl:grid-cols-4 snap-x  snap-mandatory">
         {explainInfo.map((card) => (
           <div
             key={card.title}
-            className="p-8 bg-white text-center relative overflow-hidden   flex flex-col gap-3 rounded-md  shadow-sm hover:shadow-md transition items-center w-full h-[300px] max-w-lg justify-center flex-shrink-0"
+            className="p-8 bg-white text-center relative overflow-hidden   flex flex-col gap-3 rounded-md snap-always  snap-start shadow-sm hover:shadow-md transition items-center w-[90%] max-w-lg flex-shrink-0"
           >
-            <span className="p-2 rounded-full bg-[#003580] ">{card.icon}</span>
+            <div className="relative w-16 h-16 "><Image fill alt="icon" src={card.icon} className="object-contain" /></div>
             <h3 className="text-lg text-[#003580] w-[200px] font-semibold">
               {card.title}
             </h3>
